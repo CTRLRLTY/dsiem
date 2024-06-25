ad5644c03c92588e559f3a03b45e9fbf1b87526e
ctrlrlty <52656050+CTRLRLTY@users.noreply.github.com>
2024-06-26 01:10:29 +0700

    add: a detail comment for a misleading issue
    Explained reason for the need of a code mentioned
    in 7340e2a59d8a8c207d928fe281bf442fef1e89c2

--- internal/pkg/dsiem/siem/backlogmgr.go
+++ internal/pkg/dsiem/siem/backlogmgr.go
@@ -276,6 +276,12 @@ mainLoop:
 			go func(k string) {
 				defer wg.Done()
 				// this first select is required, see #2 on https://go101.org/article/channel-closing.html
+				// Note: Detail
+				// It is required because there can exist a race condition when both chDone and
+				// incomingEvent channel receive data at the same time, which would prompt the
+				// Go runtime to randomly select the blogs.blogs[k].chData <-incomingEvent branch to run.
+				// This can lead to a running backlog go routine to eventually calling close(chDone)
+				// even tho the chDone channel is already closed.
 				select {
 				// exit early if done, this should be the case while backlog in waiting for deletion mode
 				case <-blogs.blogs[k].chDone:
